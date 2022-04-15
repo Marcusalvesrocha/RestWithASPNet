619f403a6632a90211f64b2593dc491178ba38f4
Marcusalvesrocha <sif.marcusvinicius@gmail.com>
2022-04-15 16:55:20 -0300

    Criando o Filter para os hyperlinks

---
+++ RestWithASPNet/Hypermidia/Filters/HyperMediaFilter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace RestWithASPNet.Hypermidia.Filters
+{
+    public class HyperMediaFilter : ResultFilterAttribute
+    {
+        private readonly HyperMediaFilterOptions _hyperMediaFilterOptions;
+
+        public HyperMediaFilter(HyperMediaFilterOptions hyperMediaFilterOptions)
+        {
+            _hyperMediaFilterOptions = hyperMediaFilterOptions;
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext context)
+        {
+            TryEnrichResult(context);
+            base.OnResultExecuted(context);
+        }
+
+        private void TryEnrichResult(ResultExecutedContext context)
+        {
+            if (context.Result is OkObjectResult okObjectResult)
+            {
+                var enricher = _hyperMediaFilterOptions.ContentResponseEnricherList.FirstOrDefault(x => x.CanEnrich(context));
+            };
+        }
+    }
+}
---
+++ RestWithASPNet/Hypermidia/Filters/HyperMediaFilterOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using RestWithASPNet.Hypermidia.Abstract;
+
+namespace RestWithASPNet.Hypermidia.Filters
+{
+    public class HyperMediaFilterOptions
+    {
+        public HyperMediaFilterOptions()
+        {
+        }
+
+        public List<IResponseEnricher> ContentResponseEnricherList { get; set; } = new List<IResponseEnricher>();
+    }
+}
