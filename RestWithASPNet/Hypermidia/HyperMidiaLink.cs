6809f0cb501596c0b47703cab827c6c77d6fdf96
Marcusalvesrocha <sif.marcusvinicius@gmail.com>
2022-04-15 17:31:07 -0300

    Criando a classe ContentResponseEnricher

---
+++ RestWithASPNet/Hypermidia/HyperMidiaLink.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace RestWithASPNet.Hypermidia
+{
+    public class HyperMidiaLink
+    {
+        public HyperMidiaLink()
+        {
+        }
+
+        public string Rel { get; set; }
+
+        private string href { get; set; }
+
+        public string Href {
+            get
+            {
+                object _lock = new object();
+                lock (_lock)
+                {
+                    StringBuilder sb = new StringBuilder(href);
+                    return sb.Replace("%2F", "/").ToString();
+                }
+            }
+            set {
+                href = value;
+            }
+        }
+
+        public string Type { get; set; }
+
+        public string Action { get; set; }
+    }
+}
